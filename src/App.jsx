0ed7597d75ce7af02eee7d8a6038d9c909a04855
SameerBunty <50738759+SAMEERBALANAVAR@users.noreply.github.com>
2021-03-12 12:03:59 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Services from "./Services";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route path="/" component={Home} />
-        <Route path="/" component={Home} />
-        <Route path="/" component={Home} />
-        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/services" component={Services} />
+        <Route path="/contact" component={Contact} />
       </Switch>
     </div>
   );
